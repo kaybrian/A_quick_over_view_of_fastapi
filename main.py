d4b703d6af5bca7cedd1f7186be3ba34ceb6e8f1
kayongo <kayonbrian@gmail.com>
2022-09-28 15:21:34 +0200

    query parameters

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {"data":"blog list"}
+@app.get('/blog')
+def index(limit=10,published : bool = True, sort: Optional[str]=None):
+    # only get ten published blogs
+    if published:
+        return {"data":f"blog list has only {limit} published blogs"}
+    else:
+        return {"data":"A few blogs sent out "}
 
 
 @app.get('/blog/unpublished')
@@ -27,3 +32,4 @@ def comments(blog_id):
         'data':{"comments":"there are no comments"}
     }
     
+

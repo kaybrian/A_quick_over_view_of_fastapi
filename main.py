fc244dc13cc97be293128d4de897c05107bdfdf7
kayongo <kayonbrian@gmail.com>
2022-09-28 15:06:17 +0200

    routes with swagger docs

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI
-
-
 app = FastAPI()
+
 
 @app.get('/')
 def index():
-    return {"message":"hey there"}
+    return {"data":"blog list"}
 
 
-@app.get('/about')
-def about():
+@app.get('/blog/unpublished')
+def unpublished():
+    return {"data":"Unpublished Blogs"}
+
+
+@app.get('/blog/{blog_id}')
+def show(blog_id:int):
+    # returns a blog of the same id
     return {
-        "Message":"This is a small app about fast api"
+        "Blog":f"Blog ID number : {blog_id}"
     }
+
+
+@app.get('/blog/{blog_id}/comments')
+def comments(blog_id):
+    # returns the blog comments 
+    return {
+        'data':{"comments":"there are no comments"}
+    }
+    
